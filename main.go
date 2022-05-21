1a65c36cc7b6f253648421d2bcd71910f084efae
Chris Yoxall <chris.yoxall@gmail.com>
2022-05-22 07:36:49 +1200

    Create a function.

--- main.go
+++ main.go
@@ -4,13 +4,11 @@ import "fmt"
 
 func main() {
 
-	// long form to declare variable
-	//var card string = "Ace of Spades"
-
-	// short hand way to declare variable
-	card := "Ace of Spades"
-
-	card = "Five of Diamonds"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Ace of Spaces"
 }
