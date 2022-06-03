65f6ae02016a937f81cd0a7d3e0a8d56329541b0
Chris Yoxall <chris.yoxall@gmail.com>
2022-06-04 09:10:41 +1200

    Read and write a deck to a file

--- deck.go
+++ deck.go
@@ -1,10 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // Create 'deck' type which is a slice of strings.
 type deck []string
 
+// Return string representation of deck. This is an example of a method
+// in Go, where a method is a function with a receiver argument placed
+// before the function name.
+// By convention the reference (d in this case) is a very short abbreviation.
+// While d is similar to 'this' or 'self' by convention go avoids those words.
+func (d deck) toString() string {
+
+	// Do a type conversion from deck to slice of strings
+	return strings.Join([]string(d), ", ")
+}
+
+// Create a deck of 52 cards.
 func newDeck() deck {
 
 	cardSuits := []string{"Hearts", "Diamonds", "Clubs", "Spades"}
@@ -24,11 +40,33 @@ func newDeck() deck {
 
 }
 
-// Create method by adding receiver before function name.
-// By convention the reference (d in this case) is a very short abbreviation.
-// While d is similar to 'this' or 'self' by convention go avoids those words.
+// Method to print out values in deck
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+// Return two values, both of type deck. Use slice notation to create new decks to return.
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+// Save a deck to disk
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+// Read a deck from a file
+func newDeckFromFile(filenmae string) deck {
+
+	bs, err := os.ReadFile(filenmae)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	// Convert bs to string, use split to return a slice of strings then convert to deck
+	return deck(strings.Split(string(bs), ", "))
 }
--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	cards := newDeck()
 
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
+
+	//cards.saveToFile("myCards")
+
+	cardsFromDisk := newDeckFromFile("myCards")
+	fmt.Println(cardsFromDisk)
 
 }
