1795dc3f441158a8e70c9e7f022329c5ff259fe7
Chris Yoxall <chris.yoxall@gmail.com>
2023-09-02 15:00:00 +1200

    Deadlock example.

---
+++ concurrency/deadlock/main.go
@@ -0,0 +1,25 @@
+/*
+
+Running this will result in "fatal error: all goroutines are asleep - deadlock!"
+
+Remember that both reading and writing to a channel are blocking operations in Go. When a goroutine is blocked
+control will pass to another goroutine in the hope of being able to do some work, but if all goroutines are blocked
+then you will end up with a deadlock.
+
+In this example the main goroutine is blocked on the write to the channel, and as are no other goroutines available
+it is effectively a deadlock.
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	c := make(chan string)
+	c <- "Chris"
+	fmt.Println(<-c)
+	close(c)
+
+}
