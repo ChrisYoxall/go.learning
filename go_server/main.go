51bc57601d320f8ad7329456f4a7f77a58e73bb7
Chris Yoxall <chris.yoxall@gmail.com>
2024-02-17 12:22:22 +1300

    Update http server example

--- go_server/main.go
+++ go_server/main.go
@@ -1,12 +1,16 @@
 // Simple web server. For anything more than a few responses there are plenty
 // of web frameworks to use such as Gin or Fiber.
 
+// Note that Go 1.22 introduces enhanced routing capabilities to the ServeMux handler.
+// See https://douglasmakey.medium.com/go-1-22s-http-package-updates-42aca70ceb9b
+
 package main
 
 import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -42,18 +46,37 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	fileserver := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileserver)            // called via http://localhost:8080 which will serve index.html by default
-	http.HandleFunc("/form", formHandler)   // called when submitting the form  http://localhost:8080/form.html
-	http.HandleFunc("/hello", helloHandler) // called via http://localhost:8080/hello
+	// Using the default mux (i.e. specify nil as the second argument to ListenAndServe which is often seen in simple
+	// examples) is a security concern as the default 'ServeMux' is stored in a global variable where any third party
+	// package can access it and register a route (i.e. a handlers). Instead, create a locally scoped ServeMux.
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("./static"))) // called via http://localhost:8080 which will serve index.html by default
+	mux.HandleFunc("/form", formHandler)                   // called when submitting the form  http://localhost:8080/form.html
+	mux.HandleFunc("/hello", helloHandler)                 // called via http://localhost:8080/hello
 
-	port := "8080"
+	port := 8080
 
-	fmt.Printf("Starting webserver on port %v\n", port)
+	// Could now start a server by doing 'http.ListenAndServe' as per the next commented out block. However, it is
+	// generally recommended to use the http.Server struct which provides more control over the server, and configure
+	// some time-outs to prevent slowloris attacks. Refer  https://pkg.go.dev/net/http#hdr-Servers
 
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		log.Fatal(err)
+	//fmt.Printf("Starting webserver on port %v\n", port)
+	//err := http.ListenAndServe(":"+port, mux)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
 	}
 
+	fmt.Printf("Starting webserver at %v\n", srv.Addr)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Error starting server: %v\n\n", err)
+	}
 }
